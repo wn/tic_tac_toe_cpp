d243dfcd293eb137f7f9d713acd94357f5c96369
wn <weineng.a@gmail.com>
2019-01-02 15:09:57 +0800

    Add logic

--- tic_tac_toe.cpp
+++ tic_tac_toe.cpp
@@ -6,13 +6,69 @@
 
 using namespace std;
 
-void setup() {
+bool gameOver;
 
+enum Players {
+    EMPTY, ONE, TWO
+};
+
+int INPUT;
+
+class Game {
+    public:
+        map<int, Players> game;
+        Players currentPlayer;
+
+        Game() {
+            currentPlayer = ONE;
+            game = map<int, Players>();
+        }
+
+        string valAtPos(int pos) {
+            if (!isValidPos(pos)) {
+                return "ERROR";
+            }
+            if (game[pos] == ONE) {
+                return "X";
+            } else if (game[pos] == TWO) {
+                return "0";
+            } else {
+                return to_string(pos);
+            }
+        }
+
+        static bool isValidPos(int pos) {
+            return pos <= 9 && pos >= 1;
+        }
+
+        void parseInput(int pos) {
+            if (isValidPos(pos)) {
+                game[pos] = currentPlayer;
+                swapPlayer();
+            }
+        }
+
+    private:
+        void swapPlayer() {
+            if (currentPlayer == ONE) {
+                currentPlayer = TWO;
+            } else if (currentPlayer == TWO) {
+                currentPlayer = ONE;
+            }
+        }
+};
+
+Game game;
+
+void setup() {
+    gameOver = false;
+    game = Game();
 }
 
 void draw() {
     system("clear");
     cout << "THE EXCITING GAME OF TIC-TAC-TOE!" << endl << endl;
+    cout << "It is now " << (game.currentPlayer == ONE ? "Player 1" : "Player 2") <<" turn" << endl;
     for (int i = 0; i < 13; i++) {
         for (int j = 0; j < 13; j++) {
             if (i % 4 == 0 && j % 4 == 0) {
@@ -25,7 +81,7 @@ void draw() {
                 int row = (i - 2) / 4; // 0-index
                 int col = (j + 2) / 4; // 1-index
                 int pos = row * 3 + col;
-                cout << pos;
+                cout << game.valAtPos(pos);
             } else {
                 cout << " ";
             }
@@ -36,14 +92,20 @@ void draw() {
 }
 
 void input() {
-
+    cin >> INPUT;
 }
 
 void logic() {
-
+    game.parseInput(INPUT);
 }
 
 int main() {
-    
+    setup();
+    while (!gameOver) {
+        draw();
+        input();
+        logic();
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    }
     return 0;
 }
