57df23a118c160b47df933823d1f0728b62d3cf3
wn <weineng.a@gmail.com>
2019-01-02 15:07:16 +0800

    Add drawing

--- tic_tac_toe.cpp
+++ tic_tac_toe.cpp
@@ -11,7 +11,28 @@ void setup() {
 }
 
 void draw() {
-    
+    system("clear");
+    cout << "THE EXCITING GAME OF TIC-TAC-TOE!" << endl << endl;
+    for (int i = 0; i < 13; i++) {
+        for (int j = 0; j < 13; j++) {
+            if (i % 4 == 0 && j % 4 == 0) {
+                cout << "+";
+            } else if (i % 4 == 0){
+                cout << "-";
+            } else if (j % 4 == 0) {
+                cout << "|";
+            } else if ((i + 2) % 4 == 0 && (j + 2) % 4 == 0) {
+                int row = (i - 2) / 4; // 0-index
+                int col = (j + 2) / 4; // 1-index
+                int pos = row * 3 + col;
+                cout << pos;
+            } else {
+                cout << " ";
+            }
+        }
+        cout << endl;
+    }
+    cout << endl;
 }
 
 void input() {
